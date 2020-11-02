84a2b2091a6327796f109aeef378e55c47beb46f
Adryipan <spyip3@student.monash.edu>
2020-11-02 21:52:15 +1100

    Initial commit with working Flask server for Pi to response to App connections and Connect to Firebase

---
+++ main.py
@@ -0,0 +1,159 @@
+import atexit
+
+import RPi.GPIO as GPIO
+import time
+# Import the ADS1x15 module.
+import Adafruit_ADS1x15
+import board
+import busio
+# Import the lux sensor module
+import adafruit_veml7700
+# Firebase API
+from firebase import firebase
+# Flask
+from flask import Flask
+from flaskthreads import AppContextThread
+import threading
+
+# Thread for sensor system
+thread = threading.Thread()
+
+# Firebase API key
+fireEndPoint = 'https://smart-garden-d6653.firebaseio.com'
+firebase = firebase.FirebaseApplication(fireEndPoint, None)
+
+# For setting up the ADC
+adc = Adafruit_ADS1x15.ADS1115()
+GAIN = 2 / 3
+# AO Channel for Moisture is 0
+# AO channel for Temperature is 1
+temperatureAo = 1
+moistureAo = 0
+# Lux sensor setup
+i2c = busio.I2C(board.SCL, board.SDA)
+luxSensor = adafruit_veml7700.VEML7700(i2c)
+luxA0 = 2
+
+# Moisture sensor calibration test results without soil
+# Range 20523 -> 0%, 17366 -> 100%. Assume 18935 -> 50%
+moistureZero = 19000
+
+motorPin = 7
+
+run = False
+# Time gap between each probe in second
+customMoisture = 60
+probeTime = 4
+
+
+def sensorServer_app():
+    app = Flask(__name__)
+
+    def steinhart_temperature_C(r, Ro=10000.0, To=25.0, beta=3950.0):
+        import math
+        steinhart = math.log(r / Ro) / beta  # log(R/Ro) / beta
+        steinhart += 1.0 / (To + 273.15)  # log(R/Ro) / beta + 1/To
+        steinhart = (1.0 / steinhart) - 273.15  # Invert, convert to C
+        return steinhart
+
+    def init_output(pin):
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+        GPIO.output(pin, GPIO.HIGH)
+
+    def pump_on(pump_pin=4):
+        init_output(pump_pin)
+        GPIO.output(pump_pin, GPIO.LOW)
+        time.sleep(1)
+        GPIO.output(pump_pin, GPIO.HIGH)
+
+    @app.route('/setMoisture/<newMoisture>')
+    def setCustomMoisture(newMoisture):
+        global customMoisture
+        customMoisture = int(newMoisture)
+        return 'Moisture set to ' + str(newMoisture) + '\n'
+
+    @app.route('/start')
+    def start():
+        global run
+        run = True
+        global thread
+        thread = threading.Thread(target=main, args=())
+        thread.start()
+        return 'Started\n'
+
+    @app.route('/stop')
+    def stop():
+        global run
+        run = False
+        return 'Stopped\n'
+
+    @app.route('/setProbeTime/<newProbeTime>')
+    def setProbeTime(newProbeTime):
+        global probeTime
+        probeTime = int(newProbeTime)
+        return 'New probe time set to ' + str(probeTime) + '\n'
+
+    def main():
+        print('| {0:>11} | {1:>11} | {2:>11} | {3:>11} | {4:>11} | {5:>11} |'
+              .format(*['Moisture', 'Temperature', 'Lux', 'Watered', 'Moisture threshold', 'Probing Time']))
+        global run
+        global customMoisture
+        global probeTime
+        while run:
+            record = [0] * 6
+            # Represent if water is triggered
+            record[3] = False
+            # Current watering moisture threshold
+            record[4] = customMoisture
+            # Current Probing time
+            record[5] = probeTime
+            # Calculate moisture
+            moisture = (100 - (adc.read_adc(moistureAo, gain=GAIN) / moistureZero) * 100) * 4
+            if moisture > 100:
+                moisture = 100
+            # record[moistureAo] = adc.read_adc(moistureAo, gain=GAIN)
+            record[moistureAo] = round(moisture, 2)
+            # Check if the moisture is lower than the threshold. If yes, water
+            if record[moistureAo] < customMoisture:
+                pump_on()
+                time.sleep(1)
+                GPIO.cleanup(4)
+                record[3] = True
+
+            # Calculate the temperature in Celsius
+            thermistorResistant = adc.read_adc(temperatureAo, gain=GAIN)
+            record[temperatureAo] = round(steinhart_temperature_C(thermistorResistant), 2)
+
+            # Record lux reading
+            record[luxA0] = round(luxSensor.lux, 2)
+
+            # Print result
+            print('| {0:>11} | {1:>11} | {2:>11} | {3:>11} | {4:>18} | {5:>12} |'.format(*record))
+
+            # Upload to Firebase
+            firebase.post(fireEndPoint, {
+                'Moisture': record[moistureAo],
+                'Temperature': record[temperatureAo],
+                'Lux': record[luxA0],
+                'Watered': record[3],
+                'Moisture threshold': record[4],
+                'Probing period': record[5]})
+
+            # Set gap between probe
+            time.sleep(probeTime)
+        GPIO.cleanup(4)
+
+    # Called when Flask is interrupted. If called, stop the thread and
+    def interrupt():
+        global run
+        run = False
+
+    atexit.register(interrupt)
+
+    return app
+
+
+if __name__ == '__main__':
+    app = sensorServer_app()
+    app.run(debug=True, host='0.0.0.0', port=5000)
